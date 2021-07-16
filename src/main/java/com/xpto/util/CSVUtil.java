697831329292b3f1a68f9b88adfa5400cc28f81f
Eloá Cardozo <elooa_@hotmail.com>
2021-07-16 17:48:01 -0300

    Ler Coluna e filtrar por string

--- src/main/java/com/xpto/util/CSVUtil.java
+++ src/main/java/com/xpto/util/CSVUtil.java
@@ -1,13 +1,18 @@
 package com.xpto.util;
 
+import com.opencsv.CSVReader;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.exceptions.CsvValidationException;
 import com.xpto.dominio.Cidade;
+import com.xpto.excecao.CidadeExcecao;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CSVUtil {
@@ -22,5 +27,47 @@ public class CSVUtil {
                 .withIgnoreEmptyLine(true)
                 .build();
         return csvToBean.parse();
+    }
+
+    public List<String> lerColunaCSV(String colunaQueEuQuero, String palavraQueProcuro) throws IOException, CsvValidationException, CidadeExcecao {
+
+        String file = "src/main/resources/Cidades.csv";
+        CSVReader c = new CSVReader(new FileReader(file));
+        String[] linha;
+        int contador = 0, coluna = 0;
+        List<String> stringsDaquelaColuna = new ArrayList<>();
+
+        while ((linha = c.readNext()) != null) {
+            if (contador == 0) {
+                for (int i = 0; i < linha.length; i++) {
+                    if (linha[i].equals(colunaQueEuQuero)) {
+                        coluna = i;
+                        break;
+                    }
+                    else {
+                        throw new CidadeExcecao("Coluna nao encontrada");
+                    }
+                }
+            }
+            contador++;
+            String palavraDaColuna = linha[coluna];
+            if (palavraDaColuna.contains(palavraQueProcuro)){
+                stringsDaquelaColuna.add(palavraDaColuna);
+            }
+        }
+        return stringsDaquelaColuna;
+    }
+
+    public static void main (String[] args) throws IOException, CsvValidationException {
+        CSVUtil util = new CSVUtil();
+
+
+
+        List<String> palavrasDaquelaColuna = util.lerColunaCSV
+                ("no_accents", "Sao");
+
+        for (String s : palavrasDaquelaColuna) {
+            System.out.println(s);
+        }
     }
 }
